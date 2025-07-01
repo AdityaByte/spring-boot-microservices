83ea8f4bf589c841c23d4987c1f143157f02d85a
AdityaByte <adityapawar4914@gmail.com>
2025-07-01 18:35:43 +0530

    Implemented event driven architecture via messaging service kafka

--- order-service/src/main/java/com/microservice/order/service/OrderService.java
+++ order-service/src/main/java/com/microservice/order/service/OrderService.java
@@ -3,6 +3,7 @@ package com.microservice.order.service;
 import com.microservice.order.dto.InventoryResponse;
 import com.microservice.order.dto.OrderLineItemsDto;
 import com.microservice.order.dto.OrderRequest;
+import com.microservice.order.event.OrderPlacedEvent;
 import com.microservice.order.model.Order;
 import com.microservice.order.model.OrderLineItems;
 import com.microservice.order.repository.OrderRepository;
@@ -10,6 +11,7 @@ import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,6 +29,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -56,6 +59,7 @@ public class OrderService {
 
             if (allProductsInStock) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order Placed Successfully";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later");
