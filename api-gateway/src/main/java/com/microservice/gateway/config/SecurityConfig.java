98bb5177eb9955ce3914ff2e2090192ef3b00794
AdityaByte <adityapawar4914@gmail.com>
2025-07-19 18:37:55 +0530

    added comment to api gateway for no confusion in why we are doing

--- api-gateway/src/main/java/com/microservice/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/microservice/gateway/config/SecurityConfig.java
@@ -10,10 +10,17 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+
+    // Why we are adding security here?
+    // Client sends request to the api gateway then the api gateway distributes the request by matching the
+    // url pattern.
+    // So in the below code we have done that the route "/eureka/**" only holds the static resources so we are
+    // excluding that and any other request would be authenticated.
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         return serverHttpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // csrf -> csrf.disable()
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/eureka/**")
                         .permitAll()
